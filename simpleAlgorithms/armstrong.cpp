8094ad0a5e2c3887ee8d3c3f59f98ce80ebbd716
Ariful Islam <Ariful Islam>
2022-08-06 20:47:37 +0600

    worked with some changes

---
+++ simpleAlgorithms/armstrong.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main(){
+
+  int n;cin >> n;
+  int sum = 0;
+  int original = n;
+
+  while(n>0){
+    int lastDigit = n%10;
+    sum += pow(lastDigit,3);
+    n = n/10;
+  }
+
+  if(sum == original){
+    cout << "Armstrong" << endl;
+  }
+  else{
+    cout << "Not Armstrong" << endl;
+  }
+ 
+    return 0;
+}
